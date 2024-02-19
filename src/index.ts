e28a0caaf9d9dc598b6ae6f383ba930b3e6a057b
niki1509tl <niki1509tl3@gmail.com>
2024-02-19 23:59:41 +0200

    format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import Phaser from "phaser";
 
 const config: Phaser.Types.Core.GameConfig = {
@@ -6,23 +5,23 @@ const config: Phaser.Types.Core.GameConfig = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      gravity: { y: 200 }
-    }
+      gravity: { y: 200 },
+    },
   },
   scene: {
     preload: preload,
-    create: create
-  }
+    create: create,
+  },
 };
 
 new Phaser.Game(config);
 
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
+function preload() {
+  this.load.image("sky", "assets/sky.png");
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
+function create() {
+  this.add.image(400, 300, "sky");
 }
