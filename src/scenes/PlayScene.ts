75b6e8206a418e5ecb3256e962fa80bb42b4fc14
niki1509tl <133103305+nikizetta@users.noreply.github.com>
2024-02-26 23:31:25 +0200

    debug + player and starttrigger

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,6 +1,9 @@
 import Phaser from "phaser";
 
 class PlayScene extends Phaser.Scene {
+  player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  startTrigger: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+
   get gameHeight() {
     return this.game.config.height as number;
   }
@@ -12,6 +15,16 @@ class PlayScene extends Phaser.Scene {
   create() {
     this.createEnvironment();
     this.createPlayer();
+
+    this.startTrigger = this.physics.add
+      .sprite(0, 10, null)
+      .setOrigin(0, 1)
+      .setAlpha(0);
+    this.registerPlayerControl();
+
+    this.physics.add.overlap(this.startTrigger, this.player, () => {
+      console.log('colide')
+    });
   }
 
   createEnvironment() {
@@ -19,7 +32,24 @@ class PlayScene extends Phaser.Scene {
   }
 
   createPlayer() {
-    this.physics.add.sprite(0, this.gameHeight, "dino-idle").setOrigin(0, 1);
+    this.player = this.physics.add
+      .sprite(0, this.gameHeight, "dino-idle")
+      .setOrigin(0, 1);
+
+    this.player
+      .setGravityY(5000)
+      .setCollideWorldBounds(true)
+      .setBodySize(44, 92);
+  }
+
+  registerPlayerControl() {
+    const spacebar = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+
+    spacebar.on("down", () => {
+      this.player.setVelocityY(-1600);
+    });
   }
 }
 
