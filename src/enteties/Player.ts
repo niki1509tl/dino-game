8d0cbd2357ce118e9385ef774ed5dc4b5b3014ab
niki1509tl <133103305+nikizetta@users.noreply.github.com>
2024-03-04 22:20:30 +0200

    run animation

--- src/enteties/Player.ts
+++ src/enteties/Player.ts
@@ -18,6 +18,8 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
       .setGravityY(5000)
       .setCollideWorldBounds(true)
       .setBodySize(44, 92);
+
+    this.registerAnimations();
   }
 
   update() {
@@ -28,5 +30,18 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     if (isSpaceJustDown && onFloor) {
       this.setVelocityY(-1600);
     }
+  }
+
+  playRunAnimation() {
+    this.play("dino-run", true);
+  }
+
+  registerAnimations() {
+    this.anims.create({
+      key: "dino-run",
+      frames: this.anims.generateFrameNumbers("dino-run", { start: 2, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
   }
 }
